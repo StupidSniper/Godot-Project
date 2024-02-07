68680be0f0d951aaadd88e218ae4c4eb80a22f87
paleras <olseneli7@gmail.com>
2024-02-07 13:31:35 -0800

    Update Player_Shoot.gd

--- Space_Bots/assets/scripts/Player_Shoot.gd
+++ Space_Bots/assets/scripts/Player_Shoot.gd
@@ -4,26 +4,50 @@ var damage = 1
 
 var can_shoot = true
 
+var is_reloading = false
+
+var bullet_count
+const MAX_BULLETS = 6
+const RELOAD_SPEED = 1.5
+
+var auto_reload = true
+
+
 @onready var timer = $Firerate
 @onready var raycast = $RayCast3D
 @onready var head = $"../.."
 @onready var camera = $".."
+@onready var reload_timer = $"Reload Timer"
 
 var bullet_decal = preload("res://assets/scenes/bullet_decal.tscn")
 var bullet_part = preload("res://assets/scenes/bullet_particle.tscn")
 var enemy_part = preload("res://assets/scenes/enemy_particle.tscn")
 
+func _ready():
+	bullet_count = MAX_BULLETS
+	reload_timer.wait_time = RELOAD_SPEED
+
 func _physics_process(delta):
 	
-	if Input.is_action_pressed("use") && can_shoot:
+	if Input.is_action_pressed("use") && can_shoot && bullet_count > 0:
 		can_shoot = false
 		timer.start()
 		shoot()
+		print(bullet_count)
+		
+	if auto_reload and bullet_count <= 0 && not is_reloading:
+		reload()
+		is_reloading = true
+		
+	if Input.is_action_just_pressed("reload") && is_reloading == false:
+		reload()
+		is_reloading = true
 
 func _on_firerate_timeout():
 	can_shoot = true
 
 func shoot():
+	bullet_count -= 1
 	if raycast.is_colliding():
 		var col_point = raycast.get_collision_point()
 		var normal = raycast.get_collision_normal()
@@ -61,4 +85,13 @@ func hit_non_enemy(damage, col_point, normal, target):
 	
 	bullet_decal_instantiate.look_at(col_point + normal, Vector3.UP)
 	bullet_decal_instantiate.look_at(col_point + normal, Vector3.RIGHT)
-	
+
+
+func reload():
+	reload_timer.start()
+
+
+func _on_reload_timer_timeout():
+	bullet_count = MAX_BULLETS
+	print("reloaded")
+	is_reloading = false

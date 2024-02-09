45a8ac065e6ad95dfb4c4c368447e85ed26d4f67
paleras <olseneli7@gmail.com>
2024-02-08 17:54:13 -0800

    d

--- Space_Bots/assets/scripts/Player.gd
+++ Space_Bots/assets/scripts/Player.gd
@@ -1,25 +1,24 @@
 extends CharacterBody3D
 
 #basic movement consts
-const SPRINT_SPEED = 5.5
+const SPRINT_SPEED = 4.5
 const SLIDE_BOOST = 1
 const WALLRUN_BOOST = 1
 const JUMP_FORCE = 4.5
 
 #mouse consts
-const MOUSE_SENS = 0.006
+const MOUSE_SENS = 0.005
 const MOUSE_CLAMP = Vector2(-30, 60)
 
 #gravity consts
 const GRAVITY = 9.8
 
-
 #crouch consts
-const CROUCH_SPEED = 3
+const CROUCH_SPEED = -2
 const CROUCH_HEIGHT = .7
 
 #crouch lerp consts
-const CROUCH_LERP_WEIGHT = 5
+const CROUCH_LERP_WEIGHT = 10
 
 #crouch timer reference
 @onready var crouch_timer = $"Crouch Timer"
@@ -28,24 +27,32 @@ const CROUCH_LERP_WEIGHT = 5
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
-
 #walk consts
 const WALK_SPEED = 10
+const WALK_HEIGHT = 1.0
 
 #walk lerp consts
 const MOVE_LERP_WEIGHT = 10
 
-
 #runtime variables
 
 #runtime movement variables
+
+#runtime vector3s
 var input_vec3 = Vector3()
 var movement_vector = Vector3()
 var horizontal_movement_vec = Vector3()
 var vertical_movement_vec = Vector3()
 
+#runtime bools for player states
+var is_crouched = false
+var is_sprinting = false
+
 #runtime gravity variables
 var gravity = 9.8
+
+#runtime speed variable
+var speed = 10
 
 func _ready():
 	pass
@@ -53,6 +60,7 @@ func _ready():
 
 #mouse inputs
 func _unhandled_input(event):
+	
 	#locking mouse
 	if event is InputEventMouseButton:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -73,6 +81,15 @@ func _physics_process(delta):
 	#getting input
 	var input_vec = Input.get_vector("left", "right", "up", "down")
 	input_vec3 = Vector3(input_vec.x, 0, input_vec.y).normalized()
+	
+	#crouch input
+	if Input.is_action_pressed("crouch"):
+		is_crouched = true
+	else:
+		is_crouched = false
+		
+	#calling the crouch function with the least amount of spagetti as possible
+	crouch(delta)
 	
 	#getting the horizontal movement through interpolation
 	horizontal_movement_vec = lerp(horizontal_movement_vec, input_vec3 * Vector3(WALK_SPEED, 0, WALK_SPEED), MOVE_LERP_WEIGHT * delta)
@@ -95,3 +112,15 @@ func _physics_process(delta):
 	velocity = movement_vector
 	
 	move_and_slide()
+
+#crouch lerp handling
+func crouch(delta):
+	#doign the lerp for crouch for smooth motionww
+	if is_crouched:
+		scale.y = lerp(scale.y, CROUCH_HEIGHT, CROUCH_LERP_WEIGHT * delta)
+		if scale.y <= (CROUCH_HEIGHT + 0.01):
+			scale.y = CROUCH_HEIGHT
+	else:
+		scale.y = lerp(scale.y, WALK_HEIGHT, CROUCH_LERP_WEIGHT * delta)
+		if scale.y >= (WALK_HEIGHT - 0.01):
+			scale.y = 1
